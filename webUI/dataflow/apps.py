14563094d111f741afa4cb8fe6bae3570b03be87
Haoyue Ping <hp354@drexel.edu>
2018-01-04 14:39:06 -0500

    Minor update.

--- webUI/dataflow/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataflowConfig(AppConfig):
-    name = 'dataflow'
--- webUI/preprocessing/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PreprocessingConfig(AppConfig):
-    name = 'preprocessing'
