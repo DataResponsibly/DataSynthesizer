8e84544d8470167eac362ca77f6c5b01f84b85c6
Haoyue Ping <timoping@gmail.com>
2018-11-16 18:37:03 -0500

    Remove the DataSynthesizer module in webUI.

--- webUI/DataSynthesizer/datatypes/utils/DataType.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class DataType(Enum):
-    INTEGER = 'Integer'
-    FLOAT = 'Float'
-    STRING = 'String'
-    DATETIME = 'DateTime'
-    SOCIAL_SECURITY_NUMBER = 'SocialSecurityNumber'
