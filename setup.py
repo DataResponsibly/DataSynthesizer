e35dd1033bda5cf9903d6a21be3a978c244e6fe8
Haoyue Ping <timoping@gmail.com>
2020-09-13 20:49:49 -0400

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/DataResponsibly/DataSynthesizer',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
